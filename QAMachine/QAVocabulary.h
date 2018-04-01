2a8feb99da324f5a7a5bc308989d8ec6b1a156a2
olegtaratuhin <olegtaratuhin@users.noreply.github.com>
2018-04-01 04:05:28 +0300

    Documented class WordPair
    - Class WordPair is refactored and documented

--- QAMachine/QAVocabulary.h
+++ QAMachine/QAVocabulary.h
@@ -11,28 +11,47 @@ class QAVocabulary
 {
 public:
 
-  void GenerateVocabularyFromQAset(const std::string &dataFileName, const std::string &rejectedWordsFileName, QApairsQAset & pairsQAset);
+  void GenerateVocabularyFromQAset(const std::string &dataFileName, 
+    const std::string &rejectedWordsFileName, QApairsQAset & pairsQAset);
   
   int GetWordInd(const std::string &word);
   
   size_t size();
 
-
+  /*! \class WordPair
+   *
+   *  Class WordPair holds a word that is present in database and holds it's
+   *  IDF (inverse document frequency)
+   */
   class WordPair {
   public:
-    WordPair(const std::string &s, double d)
+    /*! \brief WordPair class constructor
+     *
+     *  \param word - word
+     *  \param idf - inverse document frequency
+     */
+    explicit WordPair(const std::string &word, double idf = 0.0)
+      : word(word), idf(idf) { }
+
+    /*! \brief Setter for idf value
+     *
+     *  Allows to set custom idf to wordPair instance
+     *  Side-effect: idf changed
+     *  \param idf - new idf to set
+     */
+    void setIdf(double idf_)
     {
-      str = s;
-      idf = d;
+      idf = idf_;
     }
-    std::string str;
-    double idf;
+
+    std::string word; //!< word
+    double idf; //!< IDF (inverse document frequency)
   };
 
   WordPair& operator[](size_t ind);
   
-
   std::vector<WordPair>::iterator begin(void);
+
   std::vector<WordPair>::iterator end(void);
 
 private:
